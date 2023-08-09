0361e8c55bcdb9d007ade50b21ff0eb020dbb476
Abigail Castro <abigailestercastro@gmail.com>
2023-08-09 11:49:23 -0300

    Creacion de un archivo Java
    Hagan lo que quieran para probar. NO palabras ofensivas porfa

---
+++ PruebaGitHub/src/pruebagithub/PruebaGitHub.java
@@ -0,0 +1,12 @@
+package pruebagithub;
+
+/*
+ * @author Abigail Castro
+ */
+public class PruebaGitHub {
+
+    public static void main(String[] args) {
+        System.out.println("Hola soy Abi. Les dejo para que puedan probar el funcionamiento.");
+    }
+    
+}
