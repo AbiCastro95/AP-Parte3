ab489f108198282fe63e6d01c508281c10b70ee5
Abigail Castro <abigailestercastro@gmail.com>
2023-08-11 12:36:47 -0300

    Agregar Calendario

--- Tutorial/src/tutorial/Fechas.java
+++ Tutorial/src/tutorial/Fechas.java
@@ -28,8 +28,13 @@ public class Fechas extends javax.swing.JFrame {
         jPanel1 = new javax.swing.JPanel();
         jDFecha = new com.toedter.calendar.JDateChooser();
         jLFecha = new javax.swing.JLabel();
+        jCalendar = new com.toedter.calendar.JCalendar();
+        jLabel1 = new javax.swing.JLabel();
+        jLFechaCalendar = new javax.swing.JLabel();
+        jSeparator1 = new javax.swing.JSeparator();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Calendario");
 
         jPanel1.setForeground(new java.awt.Color(60, 63, 65));
 
@@ -40,28 +45,58 @@ public class Fechas extends javax.swing.JFrame {
         });
 
         jLFecha.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLFecha.setText("FECHA:");
+        jLFecha.setText("SELECCIONE UNA FECHA EN EL SIGUIENTE CALENDARIO");
         jLFecha.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+
+        jCalendar.addPropertyChangeListener(new java.beans.PropertyChangeListener() {
+            public void propertyChange(java.beans.PropertyChangeEvent evt) {
+                jCalendarPropertyChange(evt);
+            }
+        });
+
+        jLabel1.setText("Pruebas con Calendario");
+
+        jLFechaCalendar.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLFechaCalendar.setText("FECHA SELECCIONADA:");
+        jLFechaCalendar.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(142, 142, 142)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLFecha)
-                    .addComponent(jDFecha, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(142, Short.MAX_VALUE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(147, 147, 147)
+                        .addComponent(jLabel1))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(35, 35, 35)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jCalendar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jDFecha, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLFecha)
+                            .addComponent(jLFechaCalendar)))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(26, 26, 26)
+                        .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 430, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(34, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(58, 58, 58)
-                .addComponent(jDFecha, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(48, 48, 48)
-                .addComponent(jLFecha)
-                .addContainerGap(149, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addGap(18, 18, 18)
+                .addComponent(jLabel1)
+                .addGap(18, 18, 18)
+                .addComponent(jLFecha, javax.swing.GroupLayout.PREFERRED_SIZE, 16, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(jDFecha, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 20, Short.MAX_VALUE)
+                .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLFechaCalendar)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 13, Short.MAX_VALUE)
+                .addComponent(jCalendar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(38, 38, 38))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -80,12 +115,20 @@ public class Fechas extends javax.swing.JFrame {
 
     private void jDFechaPropertyChange(java.beans.PropertyChangeEvent evt) {//GEN-FIRST:event_jDFechaPropertyChange
         // TODO add your handling code here:
-        if (jDFecha.getDate() != null){
+        if (jDFecha.getDate() != null) {
             LocalDate fechaN = jDFecha.getDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
             DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd|MM|yyyy");
-            jLFecha.setText("FECHA: " + fechaN.format(dtf));
+            jLFecha.setText("FECHA SELECCIONADA: " + fechaN.format(dtf));
         }
     }//GEN-LAST:event_jDFechaPropertyChange
+
+    private void jCalendarPropertyChange(java.beans.PropertyChangeEvent evt) {//GEN-FIRST:event_jCalendarPropertyChange
+        // TODO add your handling code here:
+        LocalDate fechaN = jCalendar.getDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd|MM|yyyy");
+        jLFechaCalendar.setText("FECHA SELECCIONADA: " + fechaN.format(dtf));
+
+    }//GEN-LAST:event_jCalendarPropertyChange
 
     /**
      * @param args the command line arguments
@@ -123,8 +166,12 @@ public class Fechas extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private com.toedter.calendar.JCalendar jCalendar;
     private com.toedter.calendar.JDateChooser jDFecha;
     private javax.swing.JLabel jLFecha;
+    private javax.swing.JLabel jLFechaCalendar;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JSeparator jSeparator1;
     // End of variables declaration//GEN-END:variables
 }
