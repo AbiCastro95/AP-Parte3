d64ae2cb1bfa72ef4b498845d3d44ab5a800b931
Abigail Castro <abigailestercastro@gmail.com>
2023-08-14 11:26:35 -0300

    Ejercicio 2 Conversor

---
+++ Ejercicio2/src/ejercicio2/Ejercicio2.java
@@ -0,0 +1,16 @@
+package ejercicio2;
+
+/**
+ *
+ * @author Abigail
+ */
+public class Ejercicio2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
