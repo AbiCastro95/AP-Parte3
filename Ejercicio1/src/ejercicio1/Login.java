7e64a15f76be1a13080736da65a2e6fcfad11481
Abigail Castro <abigailestercastro@gmail.com>
2023-08-14 11:05:27 -0300

    Ejercicio 1 Funcionando
    Voy a investigar el botón de password por si ven cambios jaja

--- Ejercicio1/src/ejercicio1/Login.java
+++ Ejercicio1/src/ejercicio1/Login.java
@@ -5,6 +5,8 @@
  */
 package ejercicio1;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Abigail
@@ -27,21 +29,105 @@ public class Login extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        back = new javax.swing.JPanel();
+        jLTitle = new javax.swing.JLabel();
+        jLUser = new javax.swing.JLabel();
+        jTUsuario = new javax.swing.JTextField();
+        jLPassword = new javax.swing.JLabel();
+        jPassword = new javax.swing.JPasswordField();
+        jBRegistro = new javax.swing.JButton();
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        getContentPane().setLayout(new java.awt.GridLayout());
+
+        jLTitle.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLTitle.setText("INGRESO AL SISTEMA");
+        jLTitle.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        jLTitle.setOpaque(true);
+
+        jLUser.setText("USUARIO");
+
+        jTUsuario.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTUsuarioActionPerformed(evt);
+            }
+        });
+
+        jLPassword.setText("CONTRASEÑA");
+
+        jPassword.setText("jPasswordField1");
+
+        jBRegistro.setText("Registrar");
+        jBRegistro.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jBRegistroActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout backLayout = new javax.swing.GroupLayout(back);
+        back.setLayout(backLayout);
+        backLayout.setHorizontalGroup(
+            backLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, backLayout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jLTitle)
+                .addGap(136, 136, 136))
+            .addGroup(backLayout.createSequentialGroup()
+                .addGap(56, 56, 56)
+                .addGroup(backLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jBRegistro)
+                    .addGroup(backLayout.createSequentialGroup()
+                        .addGroup(backLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLUser)
+                            .addComponent(jLPassword))
+                        .addGap(42, 42, 42)
+                        .addGroup(backLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jTUsuario, javax.swing.GroupLayout.PREFERRED_SIZE, 123, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jPassword, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                .addContainerGap(100, Short.MAX_VALUE))
         );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+        backLayout.setVerticalGroup(
+            backLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(backLayout.createSequentialGroup()
+                .addGap(25, 25, 25)
+                .addComponent(jLTitle)
+                .addGap(31, 31, 31)
+                .addGroup(backLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jTUsuario, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLUser))
+                .addGap(40, 40, 40)
+                .addGroup(backLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLPassword)
+                    .addComponent(jPassword, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addComponent(jBRegistro)
+                .addContainerGap(92, Short.MAX_VALUE))
         );
+
+        getContentPane().add(back);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jTUsuarioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTUsuarioActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jTUsuarioActionPerformed
+
+    private void jBRegistroActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBRegistroActionPerformed
+        // TODO add your handling code here:
+        String user = "alumno@ulp.edu.ar";
+        String password = "12345678";
+        
+        if (jTUsuario.getText().isEmpty() || jPassword.getText().isEmpty()) {
+            JOptionPane.showMessageDialog(this, "No se admiten campos vacíos.");
+        } else if (jTUsuario.getText().equals(user) && jPassword.getText().equals(password)){
+            JOptionPane.showMessageDialog(this, "Bienvenido otra vez!");
+            jTUsuario.setText("");
+            jPassword.setText("**********");
+        } else {
+            JOptionPane.showMessageDialog(this, "Usuario y/o contraseña incorrectos.");
+        }
+    }//GEN-LAST:event_jBRegistroActionPerformed
 
     /**
      * @param args the command line arguments
@@ -79,5 +165,12 @@ public class Login extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel back;
+    private javax.swing.JButton jBRegistro;
+    private javax.swing.JLabel jLPassword;
+    private javax.swing.JLabel jLTitle;
+    private javax.swing.JLabel jLUser;
+    private javax.swing.JPasswordField jPassword;
+    private javax.swing.JTextField jTUsuario;
     // End of variables declaration//GEN-END:variables
 }
