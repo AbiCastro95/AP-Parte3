2ed7bce8435b4d607664b0842823fd066750a2b4
Abigail Castro <abigailestercastro@gmail.com>
2023-08-17 20:33:45 -0300

    Ejercicio Extra 1
    Opcional: agregar un limpiar Completas

---
+++ EjercicioExtra1/src/ejercicioextra1/Tarea.java
@@ -0,0 +1,18 @@
+package ejercicioextra1;
+
+/*
+ * @author Abigail Castro
+ */
+public class Tarea{
+
+    private String nombre;
+
+    public Tarea(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return nombre;
+    } 
+}
